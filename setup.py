b56aedb6c92e875c2911f41c7d02e0a367112ea1
Patrick S Medina <patrick.medina1@gmail.com>
2017-06-26 10:28:45 -0500

    .

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from Cython.Build import cythonize
 from numpy import get_include
 
 ext = Extension("heatmap.constructer",
-                ["heatmap.constructer.pyx"],
+                ["wingnet/heatmap/constructer.pyx"],
                 include_dirs = ['.',get_include()])
+
 setup(ext_modules = cythonize(ext))
