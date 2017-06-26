9e16340c7804e1c4c6626b267a42bf54a57e50d9
Patrick S Medina <patrick.medina1@gmail.com>
2017-06-26 14:03:00 -0500

    added functions to check / create paths

--- wingnet/classifier.py
+++ wingnet/classifier.py
@@ -1,12 +1,12 @@
 # import packages
 import tensorflow as tf
+import skimage.io as io
 import numpy as np
 import argparse
-import cv2
 import os
 
 # import modules
-import heatmap.heatmap as hmp
+# import heatmap.heatmap as hmp
 
 # GLOBAL VARIABLES
 NUM_CLASSES = 4
@@ -19,10 +19,18 @@ BB_STRIDE = 5
 
 # parse the input arguments
 parser = argparse.ArgumentParser()
-parser.add_argument('-c', '--classifydir', type=str,
+parser.add_argument('-c', '--classifydir', nargs=1, type=str, required=True,
                     help='Directory containing images to classify.')
-parser.add_argument('-s', '--savedir', type=str,
+parser.add_argument('-s', '--savedir', nargs=1, type=str, required=True,
                     help='Directory where to save the image masks.')
-args = parser.parse_args()
 
-print(args)
+# parse argument and convert to dictionary
+args = vars(parser.parse_args())
+
+# check that the image path exists
+assert os.path.isdir(args["classifydir"][0]), \
+       "Directory containing the images to classify does not exist."
+
+# create the save directory if it doesn't exist.
+if not os.path.isdir(args["savedir"][0]):
+    os.mkdir(args["savedir"][0])
