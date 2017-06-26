176b7e5ae8af2c5b556e97d221ffab3c4ed1da57
Patrick S Medina <patrick.medina1@gmail.com>
2017-06-26 12:40:10 -0500

    adding argument parser

--- wingnet/classifier.py
+++ wingnet/classifier.py
@@ -1,12 +1,12 @@
 # import packages
 import tensorflow as tf
 import numpy as np
-import timeit
+import argparse
 import cv2
 import os
 
 # import modules
-import heatmap.heatmap as heatmap
+import heatmap.heatmap as hmp
 
 # GLOBAL VARIABLES
 NUM_CLASSES = 4
@@ -16,3 +16,13 @@ IMG_CHN = 3
 BB_ROW = 32
 BB_COL = 32
 BB_STRIDE = 5
+
+# parse the input arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('-c', '--classifydir', type=str,
+                    help='Directory containing images to classify.')
+parser.add_argument('-s', '--savedir', type=str,
+                    help='Directory where to save the image masks.')
+args = parser.parse_args()
+
+print(args)
